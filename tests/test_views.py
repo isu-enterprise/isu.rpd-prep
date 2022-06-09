d64431d84ade8cb03c3b1b621cfc2d1d5d31bd0c
Evgeny Cherkashin <eugeneai@irnok.net>
2022-06-10 00:43:53 +0800

    Initial import

---
+++ tests/test_views.py
@@ -0,0 +1,13 @@
+from rpd_prep.views.default import my_view
+from rpd_prep.views.notfound import notfound_view
+
+
+def test_my_view(app_request):
+    info = my_view(app_request)
+    assert app_request.response.status_int == 200
+    assert info['project'] == 'rpd-prep'
+
+def test_notfound_view(app_request):
+    info = notfound_view(app_request)
+    assert app_request.response.status_int == 404
+    assert info == {}
