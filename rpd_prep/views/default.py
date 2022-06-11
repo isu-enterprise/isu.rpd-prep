b8457e11ea0aa51dd75329644f1a2686994400b9
Evgeny Cherkashin <eugeneai@irnok.net>
2022-06-11 10:25:57 +0800

    Added template and basic view.

--- rpd_prep/views/default.py
+++ rpd_prep/views/default.py
@@ -1,32 +1,73 @@
 from pyramid.view import view_config
 from pkg_resources import resource_stream
 from rdflib import Graph, RDF
+from common import *
+from common.kg import *
 import os.path
 
 
 class Context:
 
-    def __init__(self, name):
+    def __init__(self, name=None ):
         self._name = name
         if name is not None:
             self._stream = resource_stream(
                 'rpd_prep', os.path.join('..', 'data', name + '.ttl'))
             self.g = Graph()
+            binds(self.g)
             self.g.parse(self._stream)
+
+    def views(self):
+        """Returns a dictionary of mappings of identifiers to views
+        """
+
+        return {}
+
+    def next(self, gen):
+        return next(gen)
+
+    def __getitem__(self, index):
+        print(index)
+        return self
+
+
+    @property
+    def filename(self):
+        return self._name
+
+class Syllabus(Context):
+
+    def views(self):
+        return {"syll": self.syll}
 
     @property
     def syll(self):
         self.g.subjects(RDF.type, WPDD['Syllabus'])
 
 
+class Curriculum(Context):
+
+    def views(self):
+        return {"curr": self.curr}
+
+    @property
+    def curr(self):
+        self.g.subjects(RDF.type, IDD["Curriculum"])
+
 @view_config(route_name='home', renderer='rpd_prep:templates/mytemplate.pt')
 def my_view(request):
-    name = request.GET.get("wp", None)
-    context = Context(name)
-    if name:
-        context.about = name
 
-    return {'project': 'rpd-prep', "context": context}
+    syll = request.GET.get("wp", None)
+    curr = request.GET.get("cur", None)
+
+    syll = Syllabus(syll)
+    curr = Curriculum(curr)
+
+    vars = {}
+    for g in [syll, curr]:
+        vars.update(g.views())
+
+    return vars
 
 
 @view_config(route_name='api-1.0', request_method="POST", renderer='json')
